9e68b28eea9ba69617393b0c3318b96009ed812b
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-02-08 14:04:44 +0530

    loreum

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import { render } from "react-dom";
+import Reducers from "./components/reducers.js";
+import PostValue from "./components/connect.js";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 
+const store = createStore(Reducers)
 
-ReactDOM.render(<App />, document.getElementById("root"));
+console.log(store.getState())
+render(<Provider store={store}>
+        <PostValue />
+</Provider>, document.getElementById("root"))
